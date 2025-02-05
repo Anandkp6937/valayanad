ebe5a8507395662362da67446025bc0625330449
anandkp6937 <anandkp6937@gmail.com>
2025-02-05 11:49:16 +0530

    added notification of ulsavam

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const modelBar=document.getElementById('modelBar');
 const modelBox=document.getElementById('modelBoxx');
 const closeBtn=document.getElementById('closeBtnn');
 const addres=document.getElementById('address');
+const notificationBar=document.querySelector('.ulsavam-noti');
 //function to scale view of address
 function scaleDetails(){
 addres.style.transform='scale(0.9)';  
@@ -10,4 +11,10 @@ addres.style.transform='scale(0.9)';
  },1000);
 
 }
-
+//window load functions
+document.addEventListener('DOMContentLoaded',()=>{
+    notificationBar.style.transform='translateY(0em)';
+    setTimeout(()=>{
+        notificationBar.style.transform='translateY(-15em)';
+    },5000);
+})
