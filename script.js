de0d9bed6242ed76d3ebf3e02081774c74971d6a
anandkp6937 <anandkp6937@gmail.com>
2025-02-14 13:57:59 +0530

    removed notification header of ulsavam,and made code resuable for other events.named the file headindComponent.txt

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const modelBar=document.getElementById('modelBar');
 const modelBox=document.getElementById('modelBoxx');
 const closeBtn=document.getElementById('closeBtnn');
 const addres=document.getElementById('address');
-const notificationBar=document.querySelector('.ulsavam-noti');
+
 //function to scale view of address
 function scaleDetails(){
 addres.style.transform='scale(0.9)';  
@@ -12,8 +12,3 @@ addres.style.transform='scale(0.9)';
 
 }
 //window load functions
-document.addEventListener('DOMContentLoaded',()=>{
-    setTimeout(()=>{
-        notificationBar.style.transform='translateY(-15em)';
-    },5000);
-})
