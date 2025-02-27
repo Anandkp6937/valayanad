1fdd7255a815a2e7edb31b9bb1b74818f40e0c20
anandkp6937 <anandkp6937@gmail.com>
2025-02-27 20:18:51 +0530

    added contact details and whatapp direct chat feature

--- script.js
+++ script.js
@@ -2,13 +2,21 @@ const modelBar=document.getElementById('modelBar');
 const modelBox=document.getElementById('modelBoxx');
 const closeBtn=document.getElementById('closeBtnn');
 const addres=document.getElementById('address');
+const section7=document.getElementById('sec7');
 
 //function to scale view of address
-function scaleDetails(){
-addres.style.transform='scale(0.9)';  
- setTimeout(()=>{
-     addres.style.transform='scale(1)';
- },1000);
+function scaleDetails(e){
+    window.scrollTo({ top: document.documentElement.scrollHeight, behavior: "smooth" });
+    setTimeout(()=>{
+        window.scrollTo({ top: document.documentElement.scrollHeight, behavior: "smooth" });
+    },500)
+    addres.style.transform='scale(0.9)';   
+    section7.style.transform='scale(0.9)'
+    setTimeout(()=>{
+        addres.style.transform='scale(1)'; 
+        section7.style.transform='scale(1)'
+   
+ },1200);
 
 }
 //window load functions
