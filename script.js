af0f75ff70856c384679c9dc42b7e344daeb74f7
anandkp6937 <anandkp6937@gmail.com>
2025-04-02 13:01:01 +0530

    added theme option and feedback mail option

--- script.js
+++ script.js
@@ -3,7 +3,17 @@ const modelBox=document.getElementById('modelBoxx');
 const closeBtn=document.getElementById('closeBtnn');
 const addres=document.getElementById('address');
 const section7=document.getElementById('sec7');
+const themeBtn=document.querySelector('.theme-btn');
+const body=document.querySelector('body');
+const section8=document.querySelector('.section8');
+const whatsapp=document.querySelector('.whatsapp');
+const mailLink=document.querySelector('.link-color');
+const secSub=document.querySelector('.secSub');
+const heading=document.querySelector('.heading');
 
+let flag=0;
+//theme change callback
+themeBtn.addEventListener('click',changeTheme)
 //function to scale view of address
 function scaleDetails(e){
     window.scrollTo({ top: document.documentElement.scrollHeight, behavior: "smooth" });
@@ -19,4 +29,31 @@ function scaleDetails(e){
  },1200);
 
 }
-//window load functions
+// themechange function
+function changeTheme(){
+    if(flag===0){
+        body.style.background='#000';
+        body.style.color='#fff';
+        section8.style.background='rgb(32, 16, 44)';
+        whatsapp.style.color='yellow';
+        mailLink.style.color='yellow';
+        secSub.style.opacity='0.9';
+        heading.style.color="#fff";
+        themeBtn.style.background='#fff';
+        themeBtn.style.color="#000"
+        flag=1;
+    }
+    else{
+        body.style.background='#fff';
+        body.style.color='#000';
+        section8.style.background='#000';
+        whatsapp.style.color='green';
+        mailLink.style.color='rgba(8, 8, 102, 0.5)';
+        secSub.style.opacity='0.8';
+        heading.style.color="rgb(50, 17, 80)";
+        themeBtn.style.background='#000';
+        themeBtn.style.color="#fff";
+        flag=0;
+    }
+    
+}
